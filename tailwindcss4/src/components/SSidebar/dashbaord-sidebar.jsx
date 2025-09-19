1750423b74501cc0f79575496c89c74f68ca2ae2
JagTheFriend <JagTheFriend12@gmail.com>
2025-09-19 19:29:15 +0530

    Fixed navbar
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- tailwindcss4/src/components/SSidebar/dashbaord-sidebar.jsx
+++ tailwindcss4/src/components/SSidebar/dashbaord-sidebar.jsx
@@ -1,61 +1,65 @@
-import {Link} from "react-router-dom";
-
-import { Code2, Paperclip, Bell, ClipboardList , User , File } from "lucide-react";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarMenuItem,
-  SidebarFooter,
-  SidebarMenu,
-  SidebarHeader,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenuButton,
-  SidebarGroupLabel,
+	Bot,
+	Brain,
+	CloudUpload,
+	Cog,
+	House,
+	Sprout,
+	User,
+} from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
 } from "@/components/ui/sidebar";
 
 const elements = [
-  {
-    title: "Dashboard",
-    url: "/dashboard/dashboardPage",
-    icon: Paperclip,
-  },
-  {
-    title: "Upload",
-    url: "/dashboard/upload",
-    icon: Bell,
-  },
-  {
-    title: "Economic Data ",
-    url: "/dashboard/economic",
-    icon: User,
-  },
-  {
-    title: "History",
-    url: "/dashboard/history",
-    icon: User,
-  },
-  {
-    title: "Voice Assistant",
-    url: "/dashboard/voice",
-    icon: File,
-  },
-  {
-    title: "AI Assistant",
-    url: "/dashboard/ai",
-    icon: File,
-  },
-  {
-    title: "Profile",
-    url: "/admin/requests",
-    icon: File,
-  },
-  {
-    title: "Settings",
-    url: "/admin/requests",
-    icon: File,
-  },
-
+	{
+		title: "Dashboard",
+		url: "/dashboard/dashboardPage",
+		icon: House,
+	},
+	{
+		title: "Upload",
+		url: "/dashboard/upload",
+		icon: CloudUpload,
+	},
+	{
+		title: "Economic Data ",
+		url: "/dashboard/economic",
+		icon: Sprout,
+	},
+	{
+		title: "History",
+		url: "/dashboard/history",
+		icon: User,
+	},
+	{
+		title: "Voice Assistant",
+		url: "/dashboard/voice",
+		icon: Bot,
+	},
+	{
+		title: "AI Assistant",
+		url: "/dashboard/ai",
+		icon: Brain,
+	},
+	{
+		title: "Profile",
+		url: "/admin/requests",
+		icon: User,
+	},
+	{
+		title: "Settings",
+		url: "/admin/settings",
+		icon: Cog,
+	},
 ];
 
 // const items = [
@@ -67,40 +71,57 @@ const elements = [
 // ];
 
 export function AdminSidebar() {
-  return (
-    <Sidebar className="pt-10 [&>div]:bg-[#DCE7F8] shadow-xl">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel></SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {elements.map((ele) => (
-                <SidebarMenuItem key={ele.title}>
-                  <SidebarMenuButton asChild>
-                     <Link to={ele.url} className="flex items-center gap-3 px-3 py-2 rounded hover:bg-[#C5D7F8]">
-                      <ele.icon className="w-5 h-5 text-[#2A2A4A]" />
-                      <span className="font-madimi text-[#2A2A4A] text-[16px]">{ele.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+	const location = useLocation();
+	return (
+		<Sidebar className="pt-10 [&>div]:bg-[#fbf9f1] shadow-xl">
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel></SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{elements.map((ele) => (
+								<SidebarMenuItem key={ele.title}>
+									<SidebarMenuButton
+										asChild
+										className={
+											location.pathname === ele.url && "hover:bg-[#166700]"
+										}
+									>
+										<Link
+											to={ele.url}
+											className={`flex items-center gap-3 px-3 py-2 rounded cursor-pointer ${
+												location.pathname === ele.url ? "bg-[#166700]" : ""
+											} hover:bg-red`}
+										>
+											<ele.icon
+												className={`w-5 h-5 ${location.pathname === ele.url ? "text-white" : "text-[#2A2A4A]"}`}
+											/>
+											<span
+												className={`font-madimi ${location.pathname === ele.url ? "text-white" : "text-[#2A2A4A]"} text-[16px]`}
+											>
+												{ele.title}
+											</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
 
-        <SidebarGroup className="mt-8">
-          {/* <SidebarGroupLabel className="font-madimi
-                          
+				<SidebarGroup className="mt-8">
+					{/* <SidebarGroupLabel className="font-madimi
+
                           text-bold
                           text-[16px]
-                          
-                          
-                          text-[#2A2A4A] 
+
+
+                          text-[#2A2A4A]
                           font-large
-                          
+
                           ">Domains</SidebarGroupLabel> */}
-          <SidebarGroupContent>
-            {/* <SidebarMenu>
+					<SidebarGroupContent>
+						{/* <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
@@ -112,10 +133,10 @@ export function AdminSidebar() {
                           font-normal
                           text-bold
                           text-[16px]
-                          
-                          
+
+
                           text-[#2A2A4A]
-                          
+
                         "
                       >
                         {item.title}
@@ -125,9 +146,9 @@ export function AdminSidebar() {
                 </SidebarMenuItem>
               ))}
             </SidebarMenu> */}
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
 }
