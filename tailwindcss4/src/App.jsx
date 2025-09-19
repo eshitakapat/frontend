457fcd8cf5457aa2a4691b6d0ddd556dcb21e608
JagTheFriend <JagTheFriend12@gmail.com>
2025-09-19 19:00:51 +0530

    Fixed dashboard & upload
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- tailwindcss4/src/App.jsx
+++ tailwindcss4/src/App.jsx
@@ -1,34 +1,35 @@
-import { BrowserRouter , Routes, Route, Navigate } from "react-router-dom";
-import Layout from './components/Essentials/layout'
-import DashboardCards from './dashboard/dashboardPage'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Essentials/layout";
+import AiChatAssistant from "./dashboard/ai";
+import DashboardCards from "./dashboard/dashboardPage";
 import EconomicPage from "./dashboard/economic";
 import HistoryPage from "./dashboard/history";
 import ProfilePage from "./dashboard/profile";
 import SettingsPage from "./dashboard/settings";
 import UploadPage from "./dashboard/upload";
 import VoicePage from "./dashboard/voice";
-import AiChatAssistant from "./dashboard/ai";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-     <Layout>
-       <Routes>
-          {/* Default redirect: when user opens /dashboard, go to /dashboard/dashboardPage */}
-          <Route path="/dashboard" element={<Navigate to="/dashboard/dashboardPage" replace />} />
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					{/* Default redirect: when user opens /dashboard, go to /dashboard/dashboardPage */}
+					<Route
+						path="/dashboard"
+						element={<Navigate to="/dashboard/dashboardPage" replace />}
+					/>
 
-          <Route path="/dashboard/dashboardPage" element={<DashboardCards />} />
-          <Route path="/dashboard/economic" element={<EconomicPage />} />
-          <Route path="/dashboard/history" element={<HistoryPage />} />
-          <Route path="/dashboard/profile" element={<ProfilePage />} />
-          <Route path="/dashboard/settings" element={<SettingsPage />} />
-          <Route path="/dashboard/upload" element={<UploadPage />} />
-          <Route path="/dashboard/voice" element={<VoicePage />} />
-          <Route path="/dashboard/ai" element={<AiChatAssistant />} />
-        </Routes>
-       
-    </Layout>
-    </BrowserRouter>
-    
-  )
+					<Route path="/dashboard/dashboardPage" element={<DashboardCards />} />
+					<Route path="/dashboard/economic" element={<EconomicPage />} />
+					<Route path="/dashboard/history" element={<HistoryPage />} />
+					<Route path="/dashboard/profile" element={<ProfilePage />} />
+					<Route path="/dashboard/settings" element={<SettingsPage />} />
+					<Route path="/dashboard/upload" element={<UploadPage />} />
+					<Route path="/dashboard/voice" element={<VoicePage />} />
+					<Route path="/dashboard/ai" element={<AiChatAssistant />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
 }
