889f6898dfdcf143c182fb966258d5f332d36438
JagTheFriend <JagTheFriend12@gmail.com>
2025-09-19 19:40:50 +0530

    Fixed style
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- tailwindcss4/src/dashboard/dashboardPage.jsx
+++ tailwindcss4/src/dashboard/dashboardPage.jsx
@@ -95,7 +95,7 @@ export default function Dashboard() {
 	];
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-3 gap-8 pt-4">
+		<div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-8">
 			<div className="md:col-span-2 flex flex-col gap-8">
 				{/* Dashboard Cards */}
 				<section>
